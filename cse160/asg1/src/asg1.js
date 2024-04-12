3b7c9468e7c4d4e21c0589119f19583ba4d87292
David <darwu@ucsc.edu>
2024-04-11 22:35:30 -0700

    Cleaned up points

--- cse160/asg1/src/asg1.js
+++ cse160/asg1/src/asg1.js
@@ -1,4 +1,5 @@
-// ColoredPoint.js (c) 2012 matsuda
+// asg1.js
+
 // Vertex shader program
 var VSHADER_SOURCE = `
 attribute vec4 a_Position;
@@ -99,25 +100,19 @@ function main() {
   gl.clear(gl.COLOR_BUFFER_BIT);
 }
 
-var g_points = [];  // The array for the position of a mouse press
-var g_colors = [];  // The array to store the color of a point
-var g_sizes  = [];  // The array to store the size of a point
-
-var g_shapes = []; // The 
+var g_shapesList = []; // The array of points
 
 function click(ev) {
 
   // Extract the event click and return it in WebGL coordinates
   [x,y] = convertCoordinatesEventToGL(ev);
 
-  // Store the coordinates to g_points array
-  g_points.push([x, y]);
-
-  // Store the selected color to g_colors array
-  g_colors.push(g_selectedColor.slice());
-
-  // Store the selected size to g_sizes array
-  g_sizes.push(g_selectedSize);
+  // Create new point
+  let point = new Point();
+  point.position = [x,y];
+  point.color = g_selectedColor.slice();
+  point.size = g_selectedSize;
+  g_shapesList.push(point);
 
   // Draw every shape in g_points with respective color in g_colors
   renderAllShapes();
@@ -138,19 +133,8 @@ function renderAllShapes() {
   // Clear <canvas>
   gl.clear(gl.COLOR_BUFFER_BIT);
 
-  var len = g_points.length;
+  var len = g_shapesList.length;
   for(var i = 0; i < len; i++) {
-    var xy = g_points[i];
-    var rgba = g_colors[i];
-    var size = g_sizes[i];
-
-    // Pass the position of a point to a_Position variable
-    gl.vertexAttrib3f(a_Position, xy[0], xy[1], 0.0);
-    // Pass the color of a point to u_FragColor variable
-    gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
-    // Pass the size of a point to u_Size variable
-    gl.uniform1f(u_Size, size);
-    // Draw
-    gl.drawArrays(gl.POINTS, 0, 1);
+    g_shapesList[i].render();
   }
 }
