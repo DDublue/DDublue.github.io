67dec74b602c1b34ea506a1d08e0be067757a293
David <darwu@ucsc.edu>
2024-04-14 01:51:58 -0700

    Moved color constants to file

---
+++ cse160/asg1/src/Color.js
@@ -0,0 +1,11 @@
+// Color.js
+
+// Color constants
+const WHITE = [1.0, 1.0, 1.0, 1.0];
+const GRAY3 = [0.25,0.25,0.25,1.0];
+const GRAY2 = [0.50,0.50,0.50,1.0];
+const GRAY1 = [0.75,0.75,0.75,1.0];
+const BLACK = [0.0, 0.0, 0.0, 1.0];
+const RED   = [1.0, 0.0, 0.0, 1.0];
+const GREEN = [0.0, 1.0, 0.0, 1.0];
+const BLUE  = [0.0, 0.0, 1.0, 1.0];
