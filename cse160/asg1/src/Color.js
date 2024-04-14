fee44315f8d08146c06bc4e3d684151c73fe3911
David <darwu@ucsc.edu>
2024-04-14 16:41:19 -0700

    Added more grayscale colors

--- cse160/asg1/src/Color.js
+++ cse160/asg1/src/Color.js
@@ -1,11 +1,20 @@
 // Color.js
 
 // Color constants
-const WHITE = [1.0, 1.0, 1.0, 1.0];
-const GRAY3 = [0.25,0.25,0.25,1.0];
-const GRAY2 = [0.50,0.50,0.50,1.0];
-const GRAY1 = [0.75,0.75,0.75,1.0];
 const BLACK = [0.0, 0.0, 0.0, 1.0];
+
+const DARK2 = [0.15,0.15,0.15,1.0];
+const GRAY3 = [0.25,0.25,0.25,1.0];
+const DARK1 = [0.35,0.35,0.35,1.0];
+
+const GRAY2 = [0.50,0.50,0.50,1.0];
+
+const MID   = [0.65,0.65,0.65,1.0];
+const GRAY1 = [0.75,0.75,0.75,1.0];
+const LIGHT = [0.85,0.85,0.85,1.0];
+
+const WHITE = [1.0, 1.0, 1.0, 1.0];
 const RED   = [1.0, 0.0, 0.0, 1.0];
 const GREEN = [0.0, 1.0, 0.0, 1.0];
 const BLUE  = [0.0, 0.0, 1.0, 1.0];
+
