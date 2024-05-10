84c18d3fde96555d251cadb08b5618e9d3600170
David <darwu@ucsc.edu>
2024-05-09 18:49:36 -0700

    Increase camera FOV

--- cse160/asg3/src/Camera.js
+++ cse160/asg3/src/Camera.js
@@ -1,6 +1,6 @@
 class Camera {
   constructor() {
-    this.fov = 45;
+    this.fov = 60;
     this.eye = new Vector3([0,0,0]);
     this.at = new Vector3([0,0,-1]);
     this.up = new Vector3([0,1,0]);
